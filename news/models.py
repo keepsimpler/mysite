d005c621f94004b035e82795e049a333e109db19
keepsimpler <fengwenfeng@gmail.com>
2020-09-04 18:14:43 +0800

    News Listing Page, Pagination

--- news/models.py
+++ news/models.py
@@ -1,4 +1,5 @@
 """News listing and detail pages"""
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.db import models
 from wagtail.admin.edit_handlers import StreamFieldPanel, FieldPanel
 # from wagtail.images.blocks import ImageChooserBlock
@@ -19,9 +20,32 @@ class NewsListingPage(Page):
     def get_context(self, request, *args, **kwargs):
         """Adding custom stuff to our context"""
         context = super().get_context(request, *args, **kwargs)
+
+        # Get urls and titles of all ancestors and Self
+        ancestors = [(ancestor.url, ancestor.title) for ancestor in self.get_ancestors()[1:]]
+        ancestors += [(self.url, self.title)]
+        context['ancestors'] = ancestors
+
         # Get all posts
         all_posts = self.get_child_pages().order_by('-first_published_at')
-        # @todo add Pagination
+
+        # Paginate all posts by 20 per page
+        paginator = Paginator(all_posts, per_page=1)
+        # Try to get the ?page=x value
+        page = request.GET.get('page')
+        try:
+            # If the page exists and the ?page=x is an int
+            posts = paginator.page(page)
+        except PageNotAnInteger:
+            # If the ?page=x is not an int, show the first page
+            posts = paginator.page(1)
+        except EmptyPage:
+            # If the ?page=x is out of range (too high most likely)
+            # Then return the last page
+            posts = paginator.page(paginator.num_pages)
+
+        context['posts'] = posts
+
         return context
 
 
